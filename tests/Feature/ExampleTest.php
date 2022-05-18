3aaacb4c71b2741ce6fa6f5aa74679cb0d35a5d2
Ibrahim I. I. Musabeh <i.musabeh@imtilakgroup.com>
2022-05-18 16:28:27 +0300

    fixs bugs in load routes

---
+++ tests/Feature/ExampleTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Ibrhaim13\Translate\Tests\Unit;
+
+
+use Ibrhaim13\Translate\Tests\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function example_test(){
+        $this->assertTrue(true);
+    }
+}
