370cb6aefb3f6a8e3a1f15ed30e599687f1cef19
Rami92 <r.shehadeh@imtilak.net>
2022-05-25 17:38:25 +0300

    add faker and add localiz test

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,17 +2,17 @@
 namespace Ibrhaim13\Translate\Tests;
 use Ibrhaim13\Translate\Providers\TranslateServiceProvider;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
 use Orchestra\Testbench\TestCase as BasicTestCase;
 
 class TestCase extends BasicTestCase
 {
-    use RefreshDatabase;
+    use RefreshDatabase,WithFaker;
     protected $loadEnvironmentVariables = true;
-
     public function setUp(): void
     {
         parent::setUp();
-        // additional setup
+        $this->setUpFaker();
     }
 
     protected function getPackageProviders($app)
@@ -27,14 +27,16 @@ class TestCase extends BasicTestCase
         // import the CreatePostsTable class from the migration
         include_once __DIR__ . '/../database/migrations/create_translate_table.php';
         // run the up() method of that migration class
-        (new \TranslateTable)->up();
         $app['config']->set('database.default', 'test_translate');
         $app['config']->set('database.connections.test_translate', [
             'driver'   => 'sqlite',
             'database' => ':memory:',
             'prefix'   => '',
         ]);
-
+        (new \TranslateTable)->up();
     }
-
+    protected function generate_fake_key(): string
+    {
+       return str_replace(' ','_', implode(' ',$this->faker->words(3)));
+    }
 }
