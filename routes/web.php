7dc840eb5bd2d4d6360aabd8a35e4f7a71db9596
Ibrahim I. I. Musabeh <i.musabeh@imtilakgroup.com>
2022-05-26 15:07:01 +0300

    add translategenerate route

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,5 @@
 use Ibrhaim13\Translate\Http\Controllers\TranslateController;
 use Illuminate\Support\Facades\Route;
 
-
-Route::resource('translate', TranslateController::class);
 Route::get('translate/generate', [TranslateController::class,'generate'])->name('translate.generate');
+Route::resource('translate', TranslateController::class);
